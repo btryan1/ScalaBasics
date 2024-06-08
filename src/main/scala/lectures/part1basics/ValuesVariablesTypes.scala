8b62eb1ef0947d456388da02a35684ddb2ea5604
boxin <ben.timothy.ryan@gmail.com>
2024-06-09 00:05:52 +0100

    Part 1 of Scala Basics has begun

---
+++ src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,13 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+  // values
+  val x = 42
+  val hello = "Hello"
+  val booLean: Boolean = false
+
+  // variables
+  var x2 = 42
+  var y = x2 + 42
+  println(y)
+}
